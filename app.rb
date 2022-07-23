60a243016023993be439d63f62addb46f66c061b
christian <christianbiringanine22@gmail.com>
2022-07-23 11:50:30 +0200

    Create student

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ class App
     @book = []
     @teacher = []
     @student = []
+    @people = []
   end
 
   def create_person
@@ -22,5 +23,22 @@ class App
       puts 'Invalid option'
       nil
     end
+  end
+
+  def create_student
+    print 'Age: '
+    student_age = gets.chomp
+    print 'Name: '
+    student_name = gets.chomp
+    print 'Has parent permission? [Y/N]: '
+    parent_permission = gets.chomp
+    is_permitted = case parent_permission.downcase
+                   when 'y'
+                     true
+                   when 'n'
+                     false
+                   else
+                     puts 'It\'s a yes or no type thing!. c\'mon now! '
+                   end
   end
 end
